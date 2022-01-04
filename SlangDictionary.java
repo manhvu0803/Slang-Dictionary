ce223b238d421372e0bf3869970e1b7d6c5a2f39
Nguyễn Mạnh Vũ <93993940+nmvu19@users.noreply.github.com>
2022-01-04 11:50:35 +0700

    Add better data parsing and rename Dictionary

--- SlangDictionary.java
+++ SlangDictionary.java
@@ -1,31 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-public class Dictionary {
+public class SlangDictionary implements Closeable {
 	public class SlangNotFoundException extends Exception {}
 	public class SlangExistedException extends Exception {}
 
 	Map<String, List<String>> data;
 	
 	@SuppressWarnings("unchecked")
-	Dictionary() throws FileNotFoundException {
-		data = new HashMap<>();
+	SlangDictionary() throws IOException {
+		data = new TreeMap<>();
 
-		var file = new File("slang.dat");
-		if (!file.exists())
+		var file = new File(dataFile);
+		if (!file.exists()) {
+			System.err.println(dataFile + " not found\nReading " + defaultFile + " instead");
 			loadDefault();
+			return;
+		}
 		
 		try (var input = new ObjectInputStream(new FileInputStream(file))) {
 			data = (HashMap<String, List<String>>)input.readObject();
 		}
 		catch (Exception e) {
-			System.err.println("Error while reading slang.dat: " + e.getLocalizedMessage() + "\nReading slang.txt instead");
+			System.err.println("Error while reading " + dataFile + ": " + e.getLocalizedMessage());
+			System.err.println("\nReading " + defaultFile + " instead");
 			loadDefault();
 		}
 	}
 
-	void loadDefault() throws FileNotFoundException {
-		try (var reader = new BufferedReader(new FileReader("Slang.txt"))) {
+	void loadDefault() throws IOException {
+		try (var reader = new BufferedReader(new FileReader(defaultFile))) {
 			reader.readLine();
 			String line;
 			String last = "";
@@ -41,13 +45,14 @@ public class Dictionary {
 					data.get(last).add(items[0]);
 			}
 		}
-		catch (IOException e) {
-			System.err.println("Error while parsing dictionary: " + e.getLocalizedMessage());
-		}
 	}
 
 	public List<String> getMeaning(String slang) {
 		return data.get(slang);
+	}
+
+	public Set<String> getAllSlangs() {
+		return data.keySet();
 	}
 
 	public void addMeaning(String slang, String meaning) throws SlangNotFoundException {
@@ -70,18 +75,26 @@ public class Dictionary {
 		data.put(slang, new ArrayList<>(Arrays.asList(meanings)));
 	}
 
-	public void save() throws IOException {
-		try (var output = new ObjectOutputStream(new FileOutputStream("slang.dat"))) {
+	public void reset() throws IOException {
+		loadDefault();
+	}
+
+	@Override
+	public void close() throws IOException {
+		try (var output = new ObjectOutputStream(new FileOutputStream(dataFile))) {
 			output.writeObject(data);
 		}
 	}
-	
-	//#region static
-	static Dictionary instance = null;
 
-	public static Dictionary getInstance() throws FileNotFoundException {
+	//#region static
+	static final String defaultFile = "slang_default.txt";
+	static final String dataFile = "slang.dat";
+
+	static SlangDictionary instance = null;
+
+	public static SlangDictionary getInstance() throws IOException {
 		if (instance == null)
-			instance = new Dictionary();
+			instance = new SlangDictionary();
 		return instance;
 	}
 	//#endregion
